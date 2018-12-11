814fa1d2536bbb13e9bd9bb9cbbe289acd232fb4
Deji Kadri <dejikadri@gmail.com>
2018-12-10 16:29:15 -0800

    Removed boolean field check teporarily

--- hopper_audit/list_hopper_tables.py
+++ hopper_audit/list_hopper_tables.py
@@ -10,21 +10,25 @@ def get_table_column_dict(connection):
 
     dict_table_column = dict()
 
+    # Get list of all tables
     sql_statement = "SELECT distinct(table_name) FROM information_schema.tables WHERE table_schema='public'"
     cursor.execute(sql_statement)
     table_names = cursor.fetchall()
+    # table_names = [('instagram_post',)]
+    # table_names = [('instagram_story',), ('instagram_post',), ('instagram_meta',), ('facebook_page_insights_day',)]
+
     for table in table_names:
 
         # build a list of each tables columns
         lst_column_names = []
         sql_statement = f"SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' \
-                                AND table_name = '{table[0]}'"
+                           AND data_type <> \'boolean\'     AND table_name = '{table[0]}'"
         cursor.execute(sql_statement)
         column_names = cursor.fetchall()
         for column in column_names:
             lst_column_names.append(column[0])
 
-        # build a dictionary with tabke name as the key and the list of column names as the value
+        # build a dictionary with table names as the keys and the list of column names as the values
         dict_table_column[table[0]] = lst_column_names  # lst_columns
 
         table_columns = dict_table_column
@@ -37,14 +41,12 @@ def get_table_column_dict(connection):
                 res = check_if_column_empty(table_key, column, connection)
                 if res == 0:
                     dict_table_with_empty_column[table] = column
-                    # print('Column', column, '--->', table, 'Is empty')
                 else:
                     pass
 
     # lst_less_table_names = [x for x in lst_table_names if 'book_' in x]
     # return lst_less_table_names
 
-    # return ['facebook_page_insights_day']
     return dict_table_with_empty_column
 
 
@@ -62,8 +64,10 @@ def check_if_column_empty(table, column, connection):
     # sql_statement = f'select {column} from {table} where date_of_data > \'2018-12-01\' and {column} <> \'\'  limit 1'
     # sql_statement = f'select {column} from {table} where date_of_data > \'2018-12-01\' and {column} <> 0  limit 1'
     # sql_statement = f'select {column} from {table} where  {column} is not Null limit 1'
-    sql_statement = f'select {column} from {table} where  cast({column} as varchar) <> \'\' limit 1'
-
+    # sql_statement = f'select {column} from {table} where  cast({column} as text) <> \'\' limit 1'
+    sql_statement = f'select {column} from {table} where  {column} :: varchar <> \'\' or {column} :: varchar <> \'0\' \
+                        or {column} :: varchar is not Null  limit 1'
+    # print(sql_statement)
     cursor.execute(sql_statement)
     column_value = cursor.fetchone()
 
