c1a63c4c1b6ff7ce15e8dd6095ba321b4619ba45
Deji Kadri <dejikadri@gmail.com>
2018-12-17 11:35:06 -0800

    Added new modifiations

--- hopper_audit/list_hopper_tables.py
+++ hopper_audit/list_hopper_tables.py
@@ -1,6 +1,3 @@
-import db_conf as db
-
-
 def get_table_column_dict(connection, table_list=None):
     """
     This function loops through a list of tables and returns the below
@@ -11,40 +8,45 @@ def get_table_column_dict(connection, table_list=None):
     dict_table_column = dict()
 
     # Get list of all tables
-    sql_statement = "SELECT distinct(table_name) FROM information_schema.tables   \
-                    WHERE table_schema='public'  and  table_name like '%instagram%'"
-    cursor.execute(sql_statement)
     if table_list:
-        table_names = [(table_list,)]
+        table_names = [(table_list,) for table_list in table_list]
     else:
-        table_names = cursor.fetchall()
+        table_names = get_table_list(connection)
 
     for table in table_names:
 
         # build a list of each tables columns
         lst_column_names = []
-        sql_statement = f"SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' \
-                               AND data_type <> 'boolean' AND table_name = '{table[0]}'"
+        sql_statement = f"SELECT column_name, data_type FROM information_schema.columns WHERE table_schema = 'public' \
+                                    AND table_name = '{table[0]}'"
         cursor.execute(sql_statement)
         column_names = cursor.fetchall()
         for column in column_names:
-            lst_column_names.append(column[0])
+            lst_column_names.append((column[0], column[1]))
 
         # build a dictionary with table names as the keys and the list of column names as the values
-        dict_table_column[table[0]] = lst_column_names  # lst_columns
+        dict_table_column[table[0]] = lst_column_names
+    cursor.close()
+    dict_table_with_empty_column = build_table_column_dict(dict_table_column, connection)
 
-        table_columns = dict_table_column
+    return dict_table_with_empty_column
 
-        # loop through table column dictionary and check for empty columns and
-        # build a dictionary of table name as key  and list of  empty columns as values
-        dict_table_with_empty_column = dict()
-        lst_of_columns = []
-        for table_key, columns in table_columns.items():
-            for column in columns:
-                empty_status = check_if_column_empty(table_key, column, connection)
-                if empty_status == 0:
-                    lst_of_columns.append(column)
-                    dict_table_with_empty_column[table_key] = lst_of_columns
+
+def build_table_column_dict(table_columns_dict, connection):
+    # loop through table column dictionary and check for empty columns and
+    # build a dictionary of table name as key  and list of  empty columns as values
+    dict_table_with_empty_column = dict()
+    lst_of_columns = []
+    for table_key, columns in table_columns_dict.items():
+        for column in columns:
+            # check if the column is empty
+            empty_status = check_if_column_empty(table_key, column, connection)
+
+            # build dictionary of table name as keys and
+            # list of empty columns as values
+            if empty_status == 0:
+                lst_of_columns.append(column[0])
+                dict_table_with_empty_column[table_key] = lst_of_columns
 
     return dict_table_with_empty_column
 
@@ -57,21 +59,22 @@ def check_if_column_empty(table, column, connection):
     :param connection:
     :return: integer 0 or 1
     """
-    # connection = db.get_connection()
+
     cursor = connection.cursor()
-    # sql_statement = f'select {column} from {table} where date_of_data > \'2018-12-01\' order by {column} desc limit 1'
-    # sql_statement = f'select {column} from {table} where date_of_data > \'2018-12-01\' and {column} <> \'\'  limit 1'
-    # sql_statement = f'select {column} from {table} where date_of_data > \'2018-12-01\' and {column} <> 0  limit 1'
-    # sql_statement = f'select {column} from {table} where  {column} is not Null limit 1'
-    # sql_statement = f'select {column} from {table} where  cast({column} as text) <> \'\' limit 1'
-    sql_statement = f'select \"{column}\" from {table} where  \"{column}\" :: varchar <> \'\' or \"{column}\" :: \
-                        varchar <> \'0\' or \"{column}\" :: varchar is not Null  or  \
-                         \"{column}\" is not null and date_of_data > \'2018-12-01\' order by \"{column}\" desc limit 1'
-    # and date_of_data > \'2018-12-01\' limit 1'
+    if column[1] == 'boolean':
+        sql_statement = f'select \"{column[0]}\" from {table} where  \"{column[0]}\" is not Null limit 1'
+
+    elif column[1] in ['SMALLINT', 'INTEGER', 'BIGINT', 'NUMERIC', 'REAL', 'DOUBLE PRECISION']:
+        sql_statement = f'select \"{column[0]}\" from {table} where \"{column[0]}\" <> 0 or \
+                        \"{column[0]}\" is not Null limit 1'
+
+    else:
+        sql_statement = f'select \"{column[0]}\" from {table} where  \"{column[0]}\" :: varchar <> \'\' or \
+                        \"{column[0]}\" :: varchar is not Null  order by \"{column[0]}\" desc limit 1'
 
     cursor.execute(sql_statement)
     column_value = cursor.fetchone()
-    if column_value is not None:
+    if column_value:
         if column_value[0] == '':
             return 0
         elif column_value[0] == 0:
@@ -83,3 +86,68 @@ def check_if_column_empty(table, column, connection):
 
     cursor.close()
 
+
+def get_table_list(connection):
+    """
+        This function gets a  list of tables in the bi database
+        :return: a python list of tuples of  table names
+        """
+    cursor = connection.cursor()
+
+    # Get list of all tables
+    sql_statement = "SELECT distinct(table_name) FROM information_schema.tables   \
+                        WHERE table_schema='public'  and (table_name like 'instagram_%' or  \
+                        table_name like '%facebook_%') and table_type='BASE TABLE'"
+
+    cursor.execute(sql_statement)
+    table_names = cursor.fetchall()
+
+    return table_names
+
+
+def get_tables_without_dateofdata(connection):
+    """
+    This function returns a set of tables that do not
+    contain the field date_of_data
+    :param connection:
+    :return: set of table names
+    """
+    set_tables_without_dateofdata = set()
+
+    tables = get_table_column_dictionary(connection)
+    for tb, cl in tables.items():
+        if 'date_of_data' not in cl:
+            set_tables_without_dateofdata.add(tb)
+
+    return set_tables_without_dateofdata
+
+
+def get_table_column_dictionary(connection, table_list=None):
+    """
+    This function loops through a list of tables and returns the below
+    :return: a python dictionary with table names as key and a list of empty columns as values
+    """
+    cursor = connection.cursor()
+
+    dict_table_column = dict()
+
+    # Get list of all tables
+    if table_list:
+        table_names = [(table_list,) for table_list in table_list]
+    else:
+        table_names = get_table_list(connection)
+
+    for table in table_names:
+
+        # build a list of each tables columns
+        lst_column_names = []
+        sql_statement = f"SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' \
+                                    AND table_name = '{table[0]}'"
+        cursor.execute(sql_statement)
+        column_names = cursor.fetchall()
+        for column in column_names:
+            lst_column_names.append(column[0])
+
+        # build a dictionary with table names as the keys and the list of column names as the values
+        dict_table_column[table[0]] = lst_column_names
+    return dict_table_column
--- hopper_audit/tables_without_dataofdata.py
+++ hopper_audit/tables_without_dataofdata.py
@@ -7,12 +7,12 @@ connection = db.get_connection()
 
 start_time = time.time()
 
-dict_empty_columns = ht.get_table_column_dict(connection, ['facebook_post_metrics'])
+table_list = ht.get_tables_without_dateofdata(connection)
 end_time = int(round(time.time() * 1000))
 end_time = time.time()
 
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(dict_empty_columns)
-print("Execution Time: ", round((end_time - start_time) * 1000), "Milliseconds")
+pp.pprint(table_list)
+print("Time: ", round((end_time - start_time) * 1000), "Milliseconds")
 
 db.close_connection(connection)
